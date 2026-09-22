91786f7cc52421986b447eff708e2a6953858d8c
ahmad kayyali <ahmedkayyali2002@gmail.com>
2026-09-22 11:00:32 +0300

    first task in the file

--- JS Basic/bookstore/app.js
+++ JS Basic/bookstore/app.js
@@ -1,1 +1,16 @@
-prompt("Please Enter your name")
+let name = prompt("please enter your name:")
+let value = prompt("please enter your membership type: student/regular")
+let typebook = prompt("please enter your type book: fiction /non-fiction ")
+let titlebook = prompt("please enter specific title of the book")
+
+if (value === "student") {
+    value="Scholar"
+    alert("your book are reserved")
+    alert(`Name: ${name} \n Membership:${value} \n TitleBook:${titlebook}`)
+} else if (value === "regular") {
+    value="Member"
+    alert("your book are reserved")
+   alert(`Name: ${name} \n Membership:${value} \n TitleBook:${titlebook}`)
+} else {
+    alert(`Hi ${name} \n welcome to the website`)
+}
